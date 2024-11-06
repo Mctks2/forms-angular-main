9c0176376db5e929aa6dd2715d67a5efaac18f14
Mctks2 <mctks2@hotmail.com>
2024-11-05 23:05:14 -0300

    aula 05

--- src/app/directives/maior-idade.directive.ts
+++ src/app/directives/maior-idade.directive.ts
@@ -1,30 +1,23 @@
 import { Directive } from "@angular/core";
-import {
-  AbstractControl,
-  NG_VALIDATORS,
-  ValidationErrors,
-  Validator,
-} from "@angular/forms";
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from "@angular/forms";
 
 @Directive({
-  selector: "[maiorIdadeValidator]",
-  providers: [
-    {
-      provide: NG_VALIDATORS,
-      useExisting: MaiorIdadeDirective,
-      multi: true,
-    },
-  ],
+    selector: '[maiorIdadeValidator]',
+    providers: [{
+        provide: NG_VALIDATORS,
+        useExisting: MaiorIdadeDirective,
+        multi: true
+    }]
 })
 export class MaiorIdadeDirective implements Validator {
-  constructor() {}
-  validate(control: AbstractControl): ValidationErrors | null {
-    const dataNascimento = control.value;
-    const anoNascimento = new Date(dataNascimento).getFullYear();
-    const anoAtual = new Date().getFullYear();
-    const anoNascMais18 = anoNascimento + 18;
-    const ehMaior = anoNascMais18 <= anoAtual;
-
-    return ehMaior ? null : { maiorIdadeValidator: true };
-  }
+    
+    constructor() {}
+    validate(control: AbstractControl): ValidationErrors | null {
+        const dataNascimento = control.value;
+        const anoNascimento = new Date(dataNascimento).getFullYear();
+        const anoNascMais18 = anoNascimento + 18;
+        const anoAtual = new Date().getFullYear();
+        const ehMaior = anoNascMais18 <= anoAtual;
+        return ehMaior ? null : {'maiorIdadeValidator': true};
+    }
 }
